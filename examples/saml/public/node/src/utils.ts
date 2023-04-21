9f630b18f301a486e48adb0803c95eac8fe6f78e
Nithin Shekar Kuruba <81444731+NithinKuruba@users.noreply.github.com>
2023-04-20 19:54:36 -0700

    chore: release attempt1 (#6)
    * chore: updated git action
    
    * chore: updated readme
    
    * chore: updated readme
    
    * chore: saml playground version 1
    
    * chore: saml playground version 2
    
    * chore: saml playground version 3
    
    * chore: bump ubuntu version
    
    * chore: saml playground version 4
    
    * chore: saml playground version 5
    
    * chore: updated onload and add logoutoptions
    
    * chore: fixed login redirect uri
    
    * chore: fixed login redirect uri for vue example app
    
    * chore: updated gh action to create release
    
    * chore: pass github token to action
    
    ---------
    
    Co-authored-by: Jonathan Sharman <jon.w.sharman@gmail.com>

---
+++ examples/saml/public/node/src/utils.ts
@@ -0,0 +1,32 @@
+// https://gist.github.com/kurtsson/3f1c8efc0ccd549c9e31
+export const formatXml = (xml: string) => {
+  var formatted = '';
+  var reg = /(>)(<)(\/*)/g;
+  xml = xml.toString().replace(reg, '$1\r\n$2$3');
+  var pad = 0;
+  var nodes = xml.split('\r\n');
+  for (var n in nodes) {
+    var node = nodes[n];
+    var indent = 0;
+    if (node.match(/.+<\/\w[^>]*>$/)) {
+      indent = 0;
+    } else if (node.match(/^<\/\w/)) {
+      if (pad !== 0) {
+        pad -= 1;
+      }
+    } else if (node.match(/^<\w[^>]*[^\/]>.*$/)) {
+      indent = 1;
+    } else {
+      indent = 0;
+    }
+
+    var padding = '';
+    for (var i = 0; i < pad; i++) {
+      padding += '  ';
+    }
+
+    formatted += padding + node + '\r\n';
+    pad += indent;
+  }
+  return formatted.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/ /g, '&nbsp;');
+};
