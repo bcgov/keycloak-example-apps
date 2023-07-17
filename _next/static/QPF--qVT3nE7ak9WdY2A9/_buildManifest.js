01aafd3d1584258167f33eac33036c1a2b8ea3cd
NithinKuruba <81444731+NithinKuruba@users.noreply.github.com>
2023-07-17 03:37:14 +0000

    Deploying to gh-pages from @ bcgov/keycloak-example-apps@dc1631985c71a9f4ef46deb281950eb6375a51fe 🚀

---
+++ _next/static/QPF--qVT3nE7ak9WdY2A9/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/271-917fede7a0a8fafc.js","static/chunks/pages/index-988c50f9c8d1f5d5.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
