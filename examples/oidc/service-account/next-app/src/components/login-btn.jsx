913ec796b58564d189332fa953fc340892b1cdc0
Jon Langlois <jonathan-langlois@live.ca>
2023-09-19 12:00:33 -0700

    boilerplate next-auth app

---
+++ examples/oidc/service-account/next-app/src/components/login-btn.jsx
@@ -0,0 +1,19 @@
+import { useSession, signIn, signOut } from "next-auth/react"
+
+export default function Component() {
+  const { data: session } = useSession()
+  if (session) {
+    return (
+      <div>
+        Signed in as {session.user.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </div>
+    )
+  }
+  return (
+    <div>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
+    </div>
+  )
+}
