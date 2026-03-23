850c9b61b6936a5666550f1c61311f7382ef41af
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2026-03-23 11:13:52 -0700

    fix: export only for gh pages installation

--- playground/public-spa/next.config.js
+++ playground/public-spa/next.config.js
@@ -3,6 +3,7 @@ const BASE_PATH = process.env.APP_BASE_PATH || '';
 const nextConfig = {
   reactStrictMode: true,
   basePath: BASE_PATH,
+  output: process.env.STATIC_SITE === 'true' ? 'export' : undefined,
 };
 
 module.exports = nextConfig;
